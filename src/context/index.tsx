1d8fdfde4ef73b8eb8bc3892260b233e3b1ef403
Andrea Monroy <Andrea@MacBook-Air-de-Fiorella.local>
2022-04-06 11:49:43 -0500

    finish challenge

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -5,14 +5,6 @@ import React, { useContext } from 'react';
 export type BetContent = {
   bet: any
   setBet: (bet: any) => void
-  // titleValue: string 
-  // setTitleValue:(c: string) => void
-  // propertiesValue: string 
-  // setPropertiesValue:(c: string) => void
-  // descriptionValue: string
-  // setDescriptionValue: (c: string) => void
-  // getGltfJSON: () => void
-  // iscreated: boolean
 };
 
 export const BetContext = React.createContext<BetContent>({} as BetContent);
